9bfcb9865ade807825352e7733417413654e4df3
Lakshay Sangal <lakshay.sangal@invenio-solutions.com>
2019-11-01 11:23:58 +0000

    Second Commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,10 +7,12 @@ sap.ui.define([
 
 		onValueHelpRequested: function (oEvent) {
 			var id = oEvent.getParameters().id.split('--')[2];
+			/*var oField = new sap.ui.core.CustomData();
+			oField.setKey("field");
+			oField.setValue("id");*/
 			var oDialog = new sap.ui.xmlfragment("Fragment.Fragment.fragments.FragmentControl", this);
 			this.getView().addDependent(oDialog);
-
-			if (id == "input1") {
+		/*	if (id === "input1") {*/
 				oDialog.bindAggregation("items", {
 					path: "city>/city",
 					template: new sap.m.StandardListItem({
@@ -18,18 +20,30 @@ sap.ui.define([
 					})
 
 				});
-			} else {
-				oDialog.bindAggregation("items", {
+				/*var check = "input1";*/
+		/*	} else {*/
+				/*oDialog.bindAggregation("items", {
 					path: "country>/country",
 					template: new sap.m.StandardListItem({
 						title: "{country>name}"
 					})
 
 				});
-			}
+					var check = "input2";
+			}*/
 
 			oDialog.open();
 
+		},
+		_handleValueHelpClose: function (evt) {
+			
+			var oSelectedItem = evt.getParameter("selectedItem");
+			if (oSelectedItem) {
+				var productInput = this.byId("input1");
+				productInput.setValue(oSelectedItem.getTitle());
+			}
+			evt.getSource().getBinding("items").filter([]);
 		}
+
 	});
 });
