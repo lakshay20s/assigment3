63c1c82aa0c6976bb7b11765f943eebe78a6372a
Lakshay Sangal <lakshay.sangal@invenio-solutions.com>
2019-11-04 06:44:44 +0000

    update for both input field

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,49 +1,59 @@
+var id = "";
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+			"sap/ui/core/mvc/Controller"
+		], function (Controller) {
+			"use strict";
 
-	return Controller.extend("Fragment.Fragment.controller.View1", {
+			return Controller.extend("Fragment.Fragment.controller.View1", {
 
-		onValueHelpRequested: function (oEvent) {
-			var id = oEvent.getParameters().id.split('--')[2];
-			/*var oField = new sap.ui.core.CustomData();
-			oField.setKey("field");
-			oField.setValue("id");*/
-			var oDialog = new sap.ui.xmlfragment("Fragment.Fragment.fragments.FragmentControl", this);
-			this.getView().addDependent(oDialog);
-		/*	if (id === "input1") {*/
-				oDialog.bindAggregation("items", {
-					path: "city>/city",
-					template: new sap.m.StandardListItem({
-						title: "{city>name}"
-					})
+					onValueHelpRequested: function (oEvent) {
+						id = oEvent.getParameters().id.split('--')[2];
+						var oField = new sap.ui.core.CustomData();
+						oField.setKey("field");
+						oField.setValue("id");
+						var oDialog = new sap.ui.xmlfragment("Fragment.Fragment.fragments.FragmentControl", this);
+						this.getView().addDependent(oDialog);
+						if (id === "input1") {
+							oDialog.bindAggregation("items", {
+								path: "city>/city",
+								template: new sap.m.StandardListItem({
+									title: "{city>name}"
+								})
 
-				});
-				/*var check = "input1";*/
-		/*	} else {*/
-				/*oDialog.bindAggregation("items", {
-					path: "country>/country",
-					template: new sap.m.StandardListItem({
-						title: "{country>name}"
-					})
+							});
 
-				});
-					var check = "input2";
-			}*/
+						} else {
+							oDialog.bindAggregation("items", {
+								path: "country>/country",
+								template: new sap.m.StandardListItem({
+									title: "{country>name}"
+								})
 
-			oDialog.open();
+							});
 
-		},
-		_handleValueHelpClose: function (evt) {
-			
-			var oSelectedItem = evt.getParameter("selectedItem");
-			if (oSelectedItem) {
-				var productInput = this.byId("input1");
-				productInput.setValue(oSelectedItem.getTitle());
-			}
-			evt.getSource().getBinding("items").filter([]);
-		}
+						}
 
-	});
-});
+						oDialog.open();
+
+					},
+					_handleValueHelpClose: function (oEvent) {
+
+					
+						var oSelectedItem = oEvent.getParameter("selectedItem");
+					if (id === "input1") {
+							if (oSelectedItem) {
+								var productInput = this.byId("input1");
+								productInput.setValue(oSelectedItem.getTitle());
+							}
+							oEvent.getSource().getBinding("items").filter([]);
+						} else {
+							if (oSelectedItem) {
+								var productInput = this.byId("input2");
+								productInput.setValue(oSelectedItem.getTitle());
+							}
+							oEvent.getSource().getBinding("items").filter([]);
+						}
+					}
+
+					});
+			});
