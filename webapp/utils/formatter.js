9f6e70e7c9837b614eb4d983712ce546173756f9
Lakshay Sangal <lakshay.sangal@invenio-solutions.com>
2019-11-07 04:54:28 +0000

    formatter assignment

--- webapp/utils/formatter.js
+++ webapp/utils/formatter.js
@@ -7,8 +7,35 @@ sap.ui.define([],
 				} else {
 					return "Error";
 				}
+			},
+			city:function(place){
+			if (place === "D"){return "Delhi";}
+			else if(place === "M"){return "Mumbai";}
+			else if(place === "N"){return "Noida";}
+			else if(place === "H"){return "Hydrabad";}
+			else if(place === "P"){return "Pune";}
+			else{return "Others";}
+			},
+			dateformat : function(date){
+				var newdate = date.substring(0,2)+"/"+date.substring(2,4)+"/"+date.substring(4);
+				return newdate;
+				
 			}
 		};
 	}
+	/*,
+	function(){
+		return{
+			city:function(officecity){
+			if (officecity === "D"){return "Delhi";}
+			else if(officecity === "M"){return "Mumbai";}
+			else if(officecity === "N"){return "Noida";}
+			else if(officecity === "H"){return "Hydrabad";}
+			else if(officecity === "H"){return "Pune";}
+			else{return "Others";}
+			}	
+		};
+	}*/
+	
 
 );
